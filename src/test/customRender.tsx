a33c7c11c4c96d0cd4a70ac2edeabd117f7427ec
Duman <unknown.mann013@gmail.com>
2023-04-10 01:42:10 +0600

    redux provider for test added

--- src/test/customRender.tsx
+++ src/test/customRender.tsx
@@ -1,9 +1,11 @@
 import React, { ReactElement } from "react";
 import { render, RenderOptions } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 import { I18nextProvider } from "react-i18next";
 import ContextProvider from "../context/AppContext";
 import matchMediaSetup from "./matchMediaSetup";
+import { store } from "../app/store";
 import i18n from "../test/i18nForTests";
 
 matchMediaSetup();
@@ -12,7 +14,9 @@ const AllTheProviders = ({ children }: { children: React.ReactNode }) => {
   return (
     <MemoryRouter>
       <ContextProvider>
-        <I18nextProvider i18n={i18n}>{children}</I18nextProvider>
+        <I18nextProvider i18n={i18n}>
+          <Provider store={store}>{children}</Provider>
+        </I18nextProvider>
       </ContextProvider>
     </MemoryRouter>
   );
